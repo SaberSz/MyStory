b1ee1ab93063dd5fe573720c88a709fbe51eb1fc
abhibadass <32962622+abhibadass@users.noreply.github.com>
2018-02-05 01:40:38 +0530

    All files updated

--- f.py
+++ f.py
@@ -4,57 +4,25 @@ Created on 02-Feb-2018
 @author: dylan
 '''
 import mysql.connector as ms
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
-cnx = ms.connect(unix_socket= '/Applications/MAMP/tmp/mysql/mysql.sock', user='root', password='root', host='localhost', database='snp',)
+#cnx = ms.connect(unix_socket= '/Applications/MAMP/tmp/mysql/mysql.sock', user='root', password='root', host='localhost', database='snp',)
 
 
-@app.route('/')
-def index():
-    return '''
-    <!DOCTYPE html>
-<html>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<body>
-
-<div class="w3-container w3-green">
-  <h1>Fun Facts</h1> 
-  <p>Did you know?</p> 
-</div>
-
-<div class="w3-row-padding">
-  <div class="w3-third">
-    <h2>London</h2>
-    <p>London is the capital city of England.</p>
-    <p>It is the most populous city in the United Kingdom,
-    with a metropolitan area of over 13 million inhabitants.</p>
-  </div>
-
-  <div class="w3-third">
-    <h2>Paris</h2>
-    <p>Paris is the capital of France.</p> 
-    <p>The Paris area is one of the largest population centers in Europe,
-    with more than 12 million inhabitants.</p>
-  </div>
-
-  <div class="w3-third">
-    <h2>Tokyo</h2>
-    <p>Tokyo is the capital of Japan.</p>
-    <p>It is the center of the Greater Tokyo Area,
-    and the most populous metropolitan area in the world.</p>
-  </div>
-</div>
-
-</body>
-</html>
-    '''
 
 @app.route('/hello')
 def hello():
     return 'Hello, World'
 
-@app.route('/user/<username>')
+@app.route('/index.html')
+def login():
+   return render_template('index.html')
+
+@app.route('/home.html')
+def home():
+   return render_template('home.html')
+
+'''@app.route('/user/<username>')
 def show_user_profile(username):
     # show the user profile for that user
     return 'User %s' % username
@@ -74,7 +42,7 @@ def db_access(sk):
         d= cur.fetchall()
         
         cnx.commit()  
-        return '''
+        return
        
     <!DOCTYPE html>
 <html>
@@ -86,12 +54,12 @@ def db_access(sk):
 <h1 style="font-size:100x;color:blue;text-align:center;"> <sup>Whamen</sup> and <sub>Men</sub></h1>
 </body>
 </html>
-        '''.format(str(d))
+        .format(str(d))
         
     except ms.Error as e:
         print("Something went wrong: {}".format(e))    
     
-    
+    '''    
 if __name__ == '__main__':
     app.run(debug=True)
     
